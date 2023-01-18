7f19028e59eadd69b33da0fe4fd68d324ccc408b
zhengquantao <zhegnquantao80@gmail.com>
2023-01-18 22:01:06 +0800

    .

--- ctpbee/indicator/interface.py
+++ ctpbee/indicator/interface.py
@@ -1,10 +1,10 @@
-from .scheduler import Scheduler
+from .indicator import Indicator
 
 
 class IndicatorManager:
 
     def __init__(self):
-        self.scheduler = Scheduler()
+        self.indicator = Indicator()
 
     @property
     def inited(self):
@@ -12,28 +12,28 @@ class IndicatorManager:
         用户判断是否满足计算指标
         :return: bool
         """
-        return self.scheduler.inited
+        return self.indicator.inited
 
     @property
     def open(self):
         """
         Get open price time series.
         """
-        return self.scheduler.ret_open
+        return self.indicator.ret_open
 
     @property
     def high(self):
         """
         Get high price time series.
         """
-        return self.scheduler.ret_high
+        return self.indicator.ret_high
 
     @property
     def low(self):
         """
         Get low price time series.
         """
-        return self.scheduler.ret_low
+        return self.indicator.ret_low
 
     @property
     def close(self):
@@ -41,7 +41,7 @@ class IndicatorManager:
         Get low price time series
         :return:
         """
-        return self.scheduler.ret_close
+        return self.indicator.ret_close
 
     @property
     def volume(self):
@@ -49,7 +49,7 @@ class IndicatorManager:
         Get volume number
         :return:
         """
-        return self.scheduler.ret_volume
+        return self.indicator.ret_volume
 
     def open_csv(self, file: str, start_time=None, end_time=None):
         """
@@ -64,7 +64,7 @@ class IndicatorManager:
         :param end_time:
         :return:
         """
-        return self.scheduler.open_csv(file, start_time, end_time)
+        return self.indicator.open_csv(file, start_time, end_time)
 
     def open_json(self, file: str, start_time=None, end_time=None):
         """
@@ -81,7 +81,7 @@ class IndicatorManager:
         :param end_time:
         :return:
         """
-        return self.scheduler.open_json(file, start_time, end_time)
+        return self.indicator.open_json(file, start_time, end_time)
 
     def open_cache(self, data):
         """
@@ -91,7 +91,7 @@ class IndicatorManager:
         :param data:
         :return:
         """
-        return self.scheduler.open_cache(data)
+        return self.indicator.open_cache(data)
 
     def add_bar(self, data, opens=False):
         """
@@ -100,86 +100,83 @@ class IndicatorManager:
         :param opens: if True save file  else not save (default False)
         :return:
         """
-        self.scheduler.update_bar(data, opens)
+        self.indicator.update_bar(data, opens)
 
     def ma(self, n=15):
         if not self.inited:
             return
-        data = self.scheduler.ret_close
-        return self.scheduler.ma(data, n)
+        data = self.indicator.ret_close
+        return self.indicator.ma(data, n)
 
     def sma(self, n=15):
-        data = self.scheduler.ret_close
-        return self.scheduler.sma(data, n)
+        data = self.indicator.ret_close
+        return self.indicator.sma(data, n)
 
     def ema(self, n=12, alpha=None):
-        data = self.scheduler.ret_close
-        return self.scheduler.ema(data, n, alpha)
+        data = self.indicator.ret_close
+        return self.indicator.ema(data, n, alpha)
 
     def wma(self, n=30):
-        data = self.scheduler.ret_close
-        return self.scheduler.wma(data, n)
+        data = self.indicator.ret_close
+        return self.indicator.wma(data, n)
 
     def kd(self, n=14, f=3):
-        data = self.scheduler.ret_close
-        return self.scheduler.kd(data, n, f)
+        data = self.indicator.ret_close
+        return self.indicator.kd(data, n, f)
 
     def macd(self, n=12, m=20, f=9):
-        data = self.scheduler.ret_close
-        return self.scheduler.macd(data, n, m, f)
+        data = self.indicator.ret_close
+        return self.indicator.macd(data, n, m, f)
 
     def rsi(self, n=14, l=1):
-        data = self.scheduler.ret_close
-        return self.scheduler.rsi(data, n, l)
+        data = self.indicator.ret_close
+        return self.indicator.rsi(data, n, l)
 
     def smma(self, n=10, alpha=15):
-        data = self.scheduler.ret_close
-        return self.scheduler.smma(data, n, alpha)
+        data = self.indicator.ret_close
+        return self.indicator.smma(data, n, alpha)
 
     def atr(self, n=14):
-        data = self.scheduler.ret_close
-        return self.scheduler.atr(data, n)
+        data = self.indicator.ret_close
+        return self.indicator.atr(data, n)
 
     def stdDev(self, n=20):
-        data = self.scheduler.ret_close
-        return self.scheduler.stdDev(data, n)
+        data = self.indicator.ret_close
+        return self.indicator.stdDev(data, n)
 
     def boll(self, n=20, m=2):
-        data = self.scheduler.ret_close
-        return self.scheduler.boll(data, n, m)
+        data = self.indicator.ret_close
+        return self.indicator.boll(data, n, m)
 
     def trix(self, n=15, m=1):
-        data = self.scheduler.ret_close
-        return self.scheduler.trix(data, n, m)
+        data = self.indicator.ret_close
+        return self.indicator.trix(data, n, m)
 
     def roc(self, n=12):
-        data = self.scheduler.ret_close
-        return self.scheduler.roc(data, n)
+        data = self.indicator.ret_close
+        return self.indicator.roc(data, n)
 
     def mtm(self, n=12):
-        data = self.scheduler.ret_close
-        return self.scheduler.mtm(data, n)
+        data = self.indicator.ret_close
+        return self.indicator.mtm(data, n)
 
     def tema(self, n=25):
-        data = self.scheduler.ret_close
-        return self.scheduler.tema(data, n)
+        data = self.indicator.ret_close
+        return self.indicator.tema(data, n)
 
     def wr(self, n=14):
-        data = self.scheduler.ret_close
-        return self.scheduler.wr(data, n)
+        data = self.indicator.ret_close
+        return self.indicator.wr(data, n)
 
     def cci(self, n=20, f=0.015):
-        return self.scheduler.cci(n, f)
+        return self.indicator.cci(n, f)
 
     def sar(self, n=2, af=0.02, afmax=0.20):
-        data = self.scheduler.ret_close
-        return self.scheduler.sar(data, n, af, afmax)
+        data = self.indicator.ret_close
+        return self.indicator.sar(data, n, af, afmax)
 
     def UltimateOscillator(self):
         pass
 
     def AroonIndicator(self):
         pass
-
-    def plot(self, width=8, height=6, color="k", lw=0.5):
-        self.scheduler.plot(width=width, height=height, color=color, lw=lw)
